fb6c63cca185d5b781f3e3bfdb737c09fc9ce41c
Thales <tha.santoro2000@gmail.com>
2025-07-28 15:14:43 -0300

    testing query_params

--- views/mentees_view.py
+++ views/mentees_view.py
@@ -5,6 +5,17 @@ import numpy as np
 from datetime import date
 import locale
 
+today = date.today()
+year = today.year
+month = today.month
+month_name = today.strftime("%B")  # Retorna o nome completo do mês
+
+
+@st.cache_data
+def load_data():
+    return pegar_dados_google_sheets(month)
+
+df_final = load_data()
 
 st.title("Dashboard Individual gentlemen!!!!!!!")
 
@@ -12,3 +23,4 @@ st.title("Debug de Parâmetros de URL")
 
 # Todas as abordagens
 st.write("st.query_params:", st.query_params)
+st.dataframe(df_final)
