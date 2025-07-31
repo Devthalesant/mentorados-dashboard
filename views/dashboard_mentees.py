b705e689a42953ad50a5e0fb747c10d02d0b686c
Thales <tha.santoro2000@gmail.com>
2025-07-31 13:44:50 -0300

    testing error crroection

--- views/dashboard_mentees.py
+++ views/dashboard_mentees.py
@@ -31,7 +31,7 @@ if not st.session_state['autenticado']:
     else:
         if senha_usuario:  # usuário tentou inserir alguma senha
             st.error("Pa Acesso Negado!")
-            st.image("C:/Users/novo1/OneDrive/Desktop/Dev/Dashboard - Mentoria/image.jpg")
+            st.image("image.jpg")
         st.stop()  # interrompe a execução até que a senha seja correta
 
 else:
